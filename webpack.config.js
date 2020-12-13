e066f1062f060e176bf8ac412820e094c6b42bba
bdc5435@rit.edu <Deadlast123!>
2020-12-13 15:38:05 -0500

    setup react environment

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'index_bundle.js',
+        publicPath: '/'
+    },
+    devServer: {
+        inline: true,
+        port: 8001,
+        historyApiFallback: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                exclude: /(node_modules|bower_components)/,
+                loaders: [
+                    'babel-loader?presets[]=react,presets[]=es2015,presets[]=stage-0'
+                ]
+            }
+        ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        })
+    ]
+};
